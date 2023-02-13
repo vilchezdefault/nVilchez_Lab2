0bc5bba9d60bc1d56a0bbc7a7aae2c089368e3dd
Juan De La Cruz Vílchez Mora <vilchezdefault08@gmail.com>
2023-02-13 17:21:34 -0600

    vilchezdefault - 13/2/2023 fixed windows

---
+++ nVilchez_Lab2/FORMS/pnlDr.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace nVilchez_Lab2.FORMS
+{
+    /// <summary>
+    /// Interaction logic for pnlDr.xaml
+    /// </summary>
+    public partial class pnlDr : Window
+    {
+        public pnlDr()
+        {
+            InitializeComponent();
+        }
+    }
+}
